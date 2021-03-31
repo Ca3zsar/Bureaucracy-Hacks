dd764da4183ca042c3156f5053a7cc8599887fff
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-31 10:13:20 +0300

    refactor(MOD-1): Fix login page and make it responsive

--- Web/hacks-web/resources/views/auth/login.blade.php
+++ Web/hacks-web/resources/views/auth/login.blade.php
@@ -1,35 +1,33 @@
 @extends('layouts.auth')
 
 @section('content')
-    <section class="auth-section">
-        <div class="auth-image-container">
-            <div class="auth-image"></div>
-        </div>
-        <div class="auth-content ">
-            <form class="login-form simple-form">
-                <div class="row d-flex justify-content-center">
-                    <div class="auth-item mb-4 col-md-9 col-12">
-                        <input type="text" name="username" placeholder="username" class="input-form"/>
-                    </div>
+<section class="auth-section">
+    <div class="auth-image-container">
+        <div class="auth-image"></div>
+    </div>
+    <div class="auth-content ">
+        <form class="login-form simple-form" action="/login" method=>
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="auth-item mb-4 col-md-12 col-12">
+                    <input type="text" name="username" placeholder="username" class="input-form" />
                 </div>
-
-                <div class="auth-item mb-4 col-md-9 col-12">
-                    <input type="password" name="password" placeholder="password" class="input-form"/>
-                </div>
-
-                <div class="auth-item">
-                    <button class="auth-button">login</button>
-                </div>
-                <div class="auth-item">
-                    <button class="auth-button">register</button>
-                </div>
-            </form>
-            <div class="auth-link-container">
-                <a class="auth-link">Forgot password?</a>
             </div>
-            <div class="auth-link-container">
-                <a class="auth-link">Dont have an account?</a>
+
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="auth-item mb-4 col-md-12 col-12">
+                    <input type="password" name="password" placeholder="password" class="input-form" />
+                </div>
             </div>
+            <div class="auth-item mb-4">
+                <button class="auth-button">Login</button>
+            </div>
+            <div class="auth-item">
+                <a class="btn auth-button" href="/register">Create new account</a>
+            </div>
+        </form>
+        <div class="auth-link-container">
+            <a class="auth-link" href="/forgot-password">Forgot password?</a>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
