104899c99d79b6948a924b61fb1b31e4909ab989
Rata Cristina Iulia <cristinaiulia.rata@gmail.com>
2021-03-31 09:09:08 +0300

    Work in progress

--- Web/hacks-web/resources/views/auth/login.blade.php
+++ Web/hacks-web/resources/views/auth/login.blade.php
@@ -1,32 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="auth-section">
-    <div class="auth-image-container">
-        <div class="auth-image"></div>
-    </div>
-    <div class="auth-content ">
-        <form>
-            <div class="auth-item mb-4">
-                <input type="text" name="username" placeholder="username" class="input-form" />
-            </div>
+    <section class="auth-section">
+        <div class="auth-image-container">
+            <div class="auth-image"></div>
+        </div>
+        <div class="auth-content ">
+            <form>
+                <div class="auth-item mb-4 col-md-9 col-12">
+                    <input type="text" name="username" placeholder="username" class="input-form"/>
+                </div>
 
-            <div class="auth-item mb-4">
-                <input type="password" name="password" placeholder="password" class="input-form" />
+                <div class="auth-item mb-4 col-md-9 col-12">
+                    <input type="password" name="password" placeholder="password" class="input-form"/>
+                </div>
+                <div class="auth-item">
+                    <button class="auth-button">login</button>
+                </div>
+                <div class="auth-item">
+                    <button class="auth-button">register</button>
+                </div>
+            </form>
+            <div class="auth-link-container">
+                <a class="auth-link">Forgot password?</a>
             </div>
-            <div class="auth-item">
-                <button class="auth-button">login</button>
+            <div class="auth-link-container">
+                <a class="auth-link">Dont have an account?</a>
             </div>
-        </form>
-        <div class="auth-item">
-            <button class="auth-button">register</button>
         </div>
-        <div class="auth-link-container">
-            <a class="auth-link">Forgot password?</a>
-        </div>
-        <div class="auth-link-container">
-            <a class="auth-link">Dont have an account?</a>
-        </div>
-    </div>
-</section>
+    </section>
 @endsection
