d3522158353c244895d26c238b50ecc807f19b46
Claudiu Popa <claudiupopa330@gmail.com>
2021-03-31 10:21:54 +0300

    fix(MOD-1): Register page style

--- Web/hacks-web/resources/views/auth/login.blade.php
+++ Web/hacks-web/resources/views/auth/login.blade.php
@@ -8,21 +8,25 @@
     <div class="auth-content ">
         <form class="login-form simple-form" action="/login" method=>
             <div class="row d-flex justify-content-center align-items-center">
-                <div class="auth-item mb-4 col-md-12 col-12">
+                <div class="auth-item mb-4 col-12 col-lg-7">
                     <input type="text" name="username" placeholder="username" class="input-form" />
                 </div>
             </div>
 
             <div class="row d-flex justify-content-center align-items-center">
-                <div class="auth-item mb-4 col-md-12 col-12">
+                <div class="auth-item mb-4 col-12 col-lg-7">
                     <input type="password" name="password" placeholder="password" class="input-form" />
                 </div>
             </div>
-            <div class="auth-item mb-4">
-                <button class="auth-button">Login</button>
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="auth-item mb-4 col-12 col-lg-7">
+                    <button class="auth-button">Login</button>
+                </div>
             </div>
-            <div class="auth-item">
-                <a class="btn auth-button" href="/register">Create new account</a>
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="auth-item col-12 col-lg-7">
+                    <a class="btn auth-button" href="/register">Create new account</a>
+                </div>
             </div>
         </form>
         <div class="auth-link-container">
--- Web/hacks-web/resources/views/auth/register.blade.php
+++ Web/hacks-web/resources/views/auth/register.blade.php
@@ -1,32 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-
 <section class="auth-section">
     <div class="auth-image-container">
         <div class="auth-image"></div>
     </div>
     <div class="auth-content ">
-        <form>
-            <div class="auth-item mb-4">
-                <input type="text" name="username" placeholder="username" class="input-form" />
+        <form class="login-form simple-form" action="/login" method=>
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="auth-item mb-4 col-12 col-lg-7">
+                    <input type="text" name="username" placeholder="username" class="input-form" />
+                </div>
             </div>
 
-            <div class="auth-item mb-4">
-                <input type="password" name="password" placeholder="password" class="input-form" />
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="auth-item mb-4 col-12 col-lg-7">
+                    <input type="password" name="password" placeholder="password" class="input-form" />
+                </div>
             </div>
-            <div class="auth-item">
-                <button class="auth-button">login</button>
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="auth-item mb-4 col-12 col-lg-7">
+                    <button class="auth-button">Create account</button>
+                </div>
             </div>
         </form>
-        <div class="auth-item">
-            <button class="auth-button">register</button>
-        </div>
         <div class="auth-link-container">
-            <a class="auth-link">Forgot password?</a>
-        </div>
-        <div class="auth-link-container">
-            <a class="auth-link">Dont have an account?</a>
+            <a class="auth-link" href="/login">I already have an account</a>
         </div>
     </div>
 </section>
