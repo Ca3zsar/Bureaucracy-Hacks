35560d82c36c33b6a70992f5f1005e0eea723fd8
Cezar Todirisca <cezartodirisca@gmail.com>
2021-04-07 22:38:13 +0300

    added saving of htmls

--- IPJ_iasi/scrap_politie.py
+++ IPJ_iasi/scrap_politie.py
@@ -5,6 +5,7 @@ import codecs
 import urllib.request
 from urllib.parse import urlparse, unquote
 import os
+import shutil
 
 url = "https://is.politiaromana.ro/ro/utile/program-de-audiente"
 baseUrl = "https://is.politiaromana.ro/ro/utile/documente-eliberari-acte/"
@@ -25,10 +26,13 @@ def programAudieriPolitie():
 
     content = BeautifulSoup(page.text, "html.parser")
     programHTML = content.find_all("div", {"class": "stireDesc"})
+    
+    with open("HTMLFiles/program.html","w",encoding="utf-8") as file:
+        file.write(str(programHTML))
 
     program = re.sub('<.*?>', '', str(programHTML))
 
-    fisier = codecs.open("program_politie.txt", "w", "utf-8")
+    fisier = codecs.open("content/program_politie.txt", "w", "utf-8")
 
     fisier.write(program)
     fisier.close()
@@ -42,6 +46,11 @@ def downloadActePolitie():
         page.encoding = page.apparent_encoding
         downloadableDiv = BeautifulSoup(page.text, "html.parser")
         downloadContent = downloadableDiv.findAll('a', {"class": "numeFisier"})
+        
+        if os.path.exists(f"acte\\{folders[cont]}"):
+            shutil.rmtree(f"acte\\{folders[cont]}")
+        os.mkdir(f"acte\\{folders[cont]}")
+        
         for data in downloadContent:
             response = urllib.request.urlopen(data["href"])
 
@@ -59,6 +68,10 @@ def downloadActePolitie():
     page.encoding = page.apparent_encoding
     downloadableDiv = BeautifulSoup(page.text, "html.parser")
     downloadContent = downloadableDiv.findAll('a', {"class": "numeFisier"})
+
+    if os.path.exists("acte\\arme_explozivi"):
+        shutil.rmtree("acte\\arme_explozivi")
+    os.mkdir("acte\\arme_explozivi")
 
     for data in downloadContent:
         link = data["href"]
@@ -80,12 +93,24 @@ def downloadActePolitie():
 
 
 def getContentFromPages():
+    if os.path.exists("HTMLFiles"):
+        shutil.rmtree("HTMLFiles")
+    
+    os.mkdir("HTMLFiles")
+    
+    programAudieriPolitie()
+    downloadActePolitie()
+    
     contor = 0
     for i in sufixes:
         page = requests.get(baseUrl + i)
         page.encoding = page.apparent_encoding
         soup = BeautifulSoup(page.text, "html.parser")
         content = soup.find_all("div", {"class": "stireDesc"})
+        
+        with open(f"HTMLFiles/{folders[contor]}.html","w",encoding="utf-8") as file:
+            file.write(str(content))
+        
         content = re.sub("<.*?>", "", str(content))
         fisier = codecs.open(f"content\\{folders[contor]}.txt", "w", "utf-8")
         fisier.write(content)
