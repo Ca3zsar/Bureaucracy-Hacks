89a5804dc95ab0f105debe537f56f5a07570a4ff
murariugabriela <79075813+murariugabriela@users.noreply.github.com>
2021-04-14 21:26:43 +0300

    (backend1):delete old unfunctional program

--- IP/src/main/java/com/example/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-public interface LoginService {
-
-    public boolean login(String identifier, String password);
-}
--- IP/src/main/java/com/example/RegistrationService.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-public interface RegistrationService {
-
-    public void createUser(String email, String username, String name, String surname, String password);
-}
