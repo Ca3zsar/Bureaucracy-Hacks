f7bd7bdf5f54d0d2623065fc56cf47a298eed3b1
BenchiaRazvan <razvanbenchia9716@gmail.com>
2021-04-23 02:01:33 +0300

    Home page

--- Mobile/navBar3/app/src/main/java/com/example/navbar/ui/home/MapsFragment.kt
+++ Mobile/navBar3/app/src/main/java/com/example/navbar/ui/home/MapsFragment.kt
@@ -1,25 +1,22 @@
 package com.example.navbar.ui.home
 
+import androidx.fragment.app.Fragment
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.navbar.R
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+class MapsFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
     private val callback = OnMapReadyCallback { googleMap ->
-
         /**
          * Manipulates the map once available.
          * This callback is triggered when the map is ready to be used.
@@ -29,24 +26,17 @@ class HomeFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(47.17, 27.57)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Here"))
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
