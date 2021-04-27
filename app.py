96ce97be1d3d386079475460e01a7efee05e9a82
Cezar Todirisca <cezartodirisca@gmail.com>
2021-04-27 19:55:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # app.py
-from flask import Flask, request, jsonify,redirect, url_for
+from flask import Flask, request, jsonify, redirect, url_for
 from executor import refresh_info, get_files_list
 
 from rq import Queue
 from rq.job import Job
 from worker import conn
-import os, json
+import os
+import json
 
 app = Flask(__name__)
 DEBUG = False if 'DYNO' in os.environ else True
@@ -13,22 +14,27 @@ DEBUG = False if 'DYNO' in os.environ else True
 LOADED_DATA = 0
 DATA_LINK = ''
 
-@app.route('/refresh-info/<forced>', methods=['GET'])
-def refresh(forced):
+
+@app.route('/refresh-info/', methods=['GET'])
+def refresh():
     global LOADED_DATA, DATA_LINK
-    
-    if forced=='forced':
-      q = Queue(connection=conn)
-      job = q.enqueue_call(refresh_info,timeout=5000)
-      DATA_LINK = job.get_id()
-    else:
-      if not LOADED_DATA:
+    if not LOADED_DATA:
         q = Queue(connection=conn)
-        job = q.enqueue_call(refresh_info,timeout=5000)
+        job = q.enqueue_call(refresh_info, timeout=5000)
         LOADED_DATA = 1
         DATA_LINK = job.get_id()
-   
-    return redirect(f"https://check-diff.herokuapp.com/refresh-info/{DATA_LINK}",code=202)
+
+    return redirect(f"https://check-diff.herokuapp.com/refresh-info/{DATA_LINK}", code=202)
+
+
+@app.route('/refresh-forced', methods=['GET'])
+def refresh_forced():
+    q = Queue(connection=conn)
+    job = q.enqueue_call(refresh_info, timeout=5000)
+    LOADED_DATA = 1
+    DATA_LINK = job.get_id()
+
+    return redirect(f"https://check-diff.herokuapp.com/refresh-info/{DATA_LINK}", code=202)
 
 
 @app.route("/refresh-info/<job_key>", methods=['GET'])
@@ -41,45 +47,47 @@ def get_results(job_key):
     else:
         return "Wait!", 202
 
+
 @app.route('/get-differences/', methods=['GET'])
 def post_something():
     pass
 
 
-@app.route('/get-sites/',methods=['GET'])
+@app.route('/get-sites/', methods=['GET'])
 def get_sites():
     pass
 
 
-@app.route('/get-files/',methods=['GET'])
+@app.route('/get-files/', methods=['GET'])
 def get_files():
-  if LOADED_DATA:
-    files = get_files_list()
-  else:
-    refresh()
-    files = get_files_list()
-  
-  return jsonify(files), 200
+    if LOADED_DATA:
+        files = get_files_list()
+    else:
+        refresh()
+        files = get_files_list()
+
+    return jsonify(files), 200
 
 
 @app.route('/')
 def sign_s3():
-  # S3_BUCKET = os.getenv('S3_BUCKET_NAME')
+    # S3_BUCKET = os.getenv('S3_BUCKET_NAME')
 
-  # file_name = 'C:\\Users\\cezar\\Desktop\\dbms_sql.sql'
-  # file_type = 'sql'
+    # file_name = 'C:\\Users\\cezar\\Desktop\\dbms_sql.sql'
+    # file_type = 'sql'
 
-  # s3 = boto3.client('s3')
+    # s3 = boto3.client('s3')
 
-  # s3.upload_file(file_name,S3_BUCKET,file_name)
-    
-  # return json.dumps({
-  #   'data': presigned_post,
-  #   'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-  # })
-  
-  pass
+    # s3.upload_file(file_name,S3_BUCKET,file_name)
+
+    # return json.dumps({
+    #   'data': presigned_post,
+    #   'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
+    # })
+
+    return '<h1>Hello</h1>'
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000,debug=DEBUG)
+    app.run(threaded=True, port=5000, debug=DEBUG)
