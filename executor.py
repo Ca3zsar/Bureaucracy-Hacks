19597ae5baa960ba39d7d76b99f539265d698db9
Cezar Todirisca <cezartodirisca@gmail.com>
2021-04-29 10:14:16 +0300

    Update executor.py

--- executor.py
+++ executor.py
@@ -86,9 +86,7 @@ def add_to_S3(files,type):
 
     for file in files:
         file_name = file
-        file_name = file_name.replace(' ','+')
-        
-        file_path_S3 = f"V{VERSION}/{type}/{os.path.basename(file_name)}"
+        file_path_S3 = f"V{VERSION}/{type}/{os.path.basename(file_name.replace(' ','+'))}"
         s3.upload_file(file_name,S3_BUCKET,file_path_S3)
         os.remove(file)
         links.append(f"https://bureaucracy-files.s3.eu-central-1.amazonaws.com/{file_path_S3}")
