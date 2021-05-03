f0f6ef69750b51700a23459e694d96fff8792339
Cezar Todirisca <cezartodirisca@gmail.com>
2021-05-03 17:33:58 +0300

    Update executor.py

--- executor.py
+++ executor.py
@@ -99,13 +99,13 @@ def add_to_S3(files,type):
         file_name = file
         
         file_path_S3 = f"V{VERSION}/{type}/{os.path.basename(file_name)}"
-        file_path_S3 = file_path_S3.replace("%20"," ")
-        file_path_S3 = file_path_S3.replace("%C8%9B","t")
-        file_path_S3 = file_path_S3.replace("%C8%99","s")
-        file_path_S3 = file_path_S3.replace("%C3%A","I")
-        file_path_S3 = file_path_S3.replace("%C4%83","a")
-        file_path_S3 = file_path_S3.replace("%281%29"," ")
-        file_path_S3 = file_path_S3.replace("%281%29"," ")
+        file_path_S3 = file_path_S3.replace("\%20"," ")
+        file_path_S3 = file_path_S3.replace("\%C8\%9B","t")
+        file_path_S3 = file_path_S3.replace("\%C8\%99","s")
+        file_path_S3 = file_path_S3.replace("\%C3\%A","I")
+        file_path_S3 = file_path_S3.replace("\%C4\%83","a")
+        file_path_S3 = file_path_S3.replace("\%281\%29"," ")
+        file_path_S3 = file_path_S3.replace("\%281\%29"," ")
         
         
         s3.upload_file(file_name,S3_BUCKET,file_path_S3)
