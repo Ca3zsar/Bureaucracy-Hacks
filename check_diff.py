3451f7f4d6a06ee0652806abf8e61564c87f1c34
Cezar Todirisca <cezartodirisca@gmail.com>
2021-05-03 13:31:10 +0300

    Update check_diff.py

--- check_diff.py
+++ check_diff.py
@@ -50,6 +50,7 @@ def downloadFiles(path,s3_folder):
     s3_resource = boto3.resource('s3')
     bucket = s3_resource.Bucket(S3_BUCKET)
     for obj in bucket.objects.filter(Prefix=s3_folder):
+        print(obj.key)
         if obj.key[-1] == '/':
             continue
         bucket.download_file(obj.key, f"{path}/{os.path.basename(obj.key)}")
