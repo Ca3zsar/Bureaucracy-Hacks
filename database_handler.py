ec4c59a9018f97bfb9c68bb3ecdbe05719bcaebd
Cezar Todirisca <cezartodirisca@gmail.com>
2021-05-03 16:42:54 +0300

    Update database_handler.py

--- database_handler.py
+++ database_handler.py
@@ -5,7 +5,7 @@ def get_connection():
         host="ec2-176-34-105-15.eu-west-1.compute.amazonaws.com",
         database="d7kes16vrlnf4u",
         user="ogmoaildxvhcpo",
-        password="34a32a687974e3b06fcb2dfb47ed23f13106b199d41ad2b54ce4d23984781")
+        password="34a32a687974e3b06fcb2dfb47ed23f13106b199d41ad2b54ce4d23984781523")
     
     return conn
 
