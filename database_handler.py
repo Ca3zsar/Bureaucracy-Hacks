4c28674f281781cd9ae6cdf3b2d6cb944bc3f99e
Cezar Todirisca <cezartodirisca@gmail.com>
2021-05-03 16:33:07 +0300

    Update database_handler.py

--- database_handler.py
+++ database_handler.py
@@ -2,23 +2,23 @@ import psycopg2
 
 def get_connection():
     conn = psycopg2.connect(
-        host="ec2-54-74-14-109.eu-west-1.compute.amazonaws.com",
-        database="da385ubja6sllm",
-        user="bvvqslqwgcnbiy",
-        password="d39f9c8696f33a870b7ef5cff9399172613ce7a0649bbac57be63f713ca78c9b")
+        host="ec2-176-34-105-15.eu-west-1.compute.amazonaws.com",
+        database="d7kes16vrlnf4u",
+        user="ogmoaildxvhcpo",
+        password="34a32a687974e3b06fcb2dfb47ed23f13106b199d41ad2b54ce4d23984781")
     
     return conn
 
 def add_to_database(params):
     connection = get_connection()
     
-    statement = "INSERT INTO files VALUES(%s, %s)"
-    delete_statement = "DELETE FROM files"
+    statement = "INSERT INTO anexe VALUES(%s, %s)"
+    delete_statement = "DELETE FROM anexe"
     cursor = connection.cursor()
     
     cursor.execute(delete_statement)
     for key in list(params.keys()):
-        statement = f"INSERT INTO files (file_name, file_url) VALUES('{key}', '{params[key]}')"
+        statement = f"INSERT INTO anexe (filename, file_link) VALUES('{os.path.splitext(key)[0]}', '{params[key]}')"
         cursor.execute(statement)
         
     connection.commit()
