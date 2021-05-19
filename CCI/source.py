9d9e202f15c338a479db3761f8c88bfe3245ad50
stefanpogor <pogorstefan@gmail.com>
2021-05-19 22:20:07 +0300

    (backend2) : finished CCI scrapper

--- CCI/source.py
+++ CCI/source.py
@@ -7,6 +7,7 @@ import shutil
 
 url = "http://www.cciasi.ro/documente_comerciale.htm"
 root = "http://www.cciasi.ro/"
+download_link="http://www.cciasi.ro/doc/macheta_CO.doc"
 
 director = f"{os.path.dirname(__file__)}/Content"
 HTMLFiles = f"{os.path.dirname(__file__)}/HTMLFiles"
@@ -27,39 +28,67 @@ def makeDirectors():
     os.mkdir(HTMLFiles)
     os.mkdir(acte)
     
-def fillContent(section):
-    with open(f"{HTMLFiles}/cci.html","w",encoding="utf-8") as file:
-        file.write(str(section))
+def getData(var):
+    htmlToText = str(var)
+    occurrence=9
+    table_9th_pos=[m.start() for m in re.finditer(r"<table", htmlToText)]
+    end_table=[m.start() for m in re.finditer(r"</table>", htmlToText)]
+
+    end_text = ''
+    if len(end_table) >= 3:
+        for i in range(end_table[2], len(htmlToText)):
+            end_text += htmlToText[i]
+    else:
+        print("No {} occurrence of substring lies in given string".format(3))
     
+    htmlToText = str(var)
+    textToHtml = ''
+    if len(table_9th_pos) >= 9:
+        for i in range(table_9th_pos[occurrence-1], len(htmlToText)-len(end_text)):
+            textToHtml += htmlToText[i]
+    else:
+        print("No {} occurrence of substring lies in given string".format(occurrence))
 
-def downloadHrefs(soup):
-    for a in soup.select('table'):
-    
-        toDownload = a.get('href')
-        if not toDownload:
-            continue
-        
-        toDownload = toDownload[2:]
-        if toDownload.startswith("/"):
-            toDownload = root + toDownload[1:]
+    new_html = BeautifulSoup(textToHtml, 'html.parser')
 
-        baseName = os.path.basename(toDownload)
+    #writing content to .txt file in Content
+    sep='-------------------------------'
+    cci_text=''
+    tr_tags=new_html.findAll("tr")
+    row=''
+    for tr_tag in tr_tags:
+        td_tags=tr_tag.findAll("td")
+        for td_tag in td_tags:
+            row=td_tag.text
+            row=re.sub("\s(\s)+", " ", row)
+            cci_text+=row+" | "
+        cci_text+="\n" + sep + "\n"
 
-        req = requests.get(toDownload, allow_redirects=True)
+    with open(f"{director}\\Camera_de_Comert_si_Industrie_Iasi.txt", "w", encoding="utf-8") as file:
+        file.write(cci_text)
 
-        open(f"{acte}/{baseName}", 'wb').write(req.content)
+    #writing content to .html file in HTMLFiles
+    with open(f"{HTMLFiles}\\Camera_de_Comert_si_Industrie_Iasi.html", "w", encoding="utf-8") as file:
+        file.write(str(new_html))
 
+    #download file
+    source=requests.get(download_link)
+    baseName = os.path.basename(download_link)
+    f = open (acte + '/' + baseName, 'wb')
+    try:
+        f.write(source.content)
+    except:
+        f.close(source.content)
+    f.close()
 
 def main():
     deletingFiles()
     makeDirectors()
 
-    source = requests.get(url).text
-    soup = BeautifulSoup(source, 'lxml')
-    section = soup.select('.listaform')
+    source = requests.get(url)
+    soup = BeautifulSoup(source.content, 'html.parser')
 
-    fillContent(section)
-    downloadHrefs(soup)
+    getData(soup)
 
 if __name__=="__main__":
     main()
